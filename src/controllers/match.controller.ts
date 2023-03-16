57e1c987dba98e99be40c94cc0359c17a52426f6
diegomated1 <diegocardwr@gmail.com>
2023-03-16 14:00:47 -0500

    using match model

---
+++ src/controllers/match.controller.ts
@@ -0,0 +1,22 @@
+import {Server, Socket} from 'socket.io';
+import ui from 'uniqid';
+import GameModel from '../models/game.model.js';
+
+const matchController = (io: Server, socket: Socket) => {
+
+    const matchCreate = async (id_user:string, number_players:Number, bet:number)=>{
+        const id_game = ui.process(); 
+        const new_game = await GameModel.create({
+            _id: id_game,
+            id_owner: id_user,
+            max_number_players: number_players,
+            min_bet: bet,
+            players: {id_user, bet}
+        });
+        io.emit("match:create", new_game.toJSON());
+    }
+
+    socket.on("match:create", matchCreate);
+};
+
+export default matchController;
