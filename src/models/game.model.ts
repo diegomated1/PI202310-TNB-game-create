a66df39b4d6e350de149833f860bd49fc49e58c2
diegomated1 <diegocardwr@gmail.com>
2023-03-16 13:59:28 -0500

    match model

---
+++ src/models/game.model.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import db from '../database/mongo.db.js';
+
+const GameModel = db.model('games', new mongoose.Schema({
+    _id: String,
+    id_owner: String,
+    max_number_players: Number,
+    players: Array<{id_user: String, bet: Number}>,
+    min_bet: Number
+}));
+
+export default GameModel;
