191bc98d3644b3b8089030c0c503d0bd8dae38e6
diegomated1 <diegocardwr@gmail.com>
2023-04-02 15:51:21 -0500

    lobby router

---
+++ src/router/lobby.router.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import LobbyController from "../controllers/lobby.controller.js";
+
+export default class LobbyRouter{
+
+    router: Router
+    private controller: LobbyController
+
+    constructor(){
+        this.router = Router();
+        this.controller = new LobbyController();
+        this.routes();
+    }
+
+    private routes(){
+        this.router.route('/').post(this.controller.createLobby);
+        this.router.route('/').get(this.controller.get);
+        this.router.route('/:id_lobby').get(this.controller.getById);
+    }
+}
